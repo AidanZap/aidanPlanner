a4af2e3071f361fa36c25c26e5e7bd9170b96a63
AidanZap <aidanzduck@gmail.com>
2021-01-24 19:17:57 -0500

    deploys

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.set('Cache-Control', 'private, max-age=300, s-maxage=600');
+});
+
+app.get('/test', (req, res) => {
+    res.send("testing, testing");
+})
+
+exports.app = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
